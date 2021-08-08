0b5208369185196da3ac44fead31221d5a21bfd4
Zac <zacharyflynn06@gmail.com>
2021-08-07 18:45:34 -0600

    add contact infor

--- src/features/contact/Contact.js
+++ src/features/contact/Contact.js
@@ -4,7 +4,14 @@ const Contact = (props) => {
     return (
         <div>
             <h1>Contact Page</h1>
-            <p>some info to contact us</p>
+            <h2>Booking</h2>
+            <h3>Jordan Carriere</h3>
+            <h3>jordan@alpine-partners.net</h3>
+
+            <h2>Management</h2>
+            <h3>Tynan Conroy</h3>
+            <h3>Mind Warp Management</h3>
+
         </div>
     )
 }
--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+
 const Home = (props) => {
     return (
         <div>
             <h1>Eminence Ensemble</h1>
+            <img url="public/images/logo.png" alt="logo" />
         </div>
     )
 }
