8225dc5dd2f8e8c5dcd5f109ad66ca933e756867
Zac <zacharyflynn06@gmail.com>
2021-08-08 23:50:24 -0600

    add logo to home screen

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import NavBar from '../navbar/NavBar';
-import styles from './Home.module.css'
-
+import style from './Home.module.css'
+import logo from './logo.png'
 
 const Home = (props) => {
     return (
-        <div className={styles.homeContainer}>
-            <div className={styles.title_container}>
-                <h1 className={styles.title}>Eminence Ensemble</h1>
-                {/* <img src="src/logo.png" alt="logo" /> */}
+        <div className={style.homeContainer}>
+            <div className={style.title_container}>
+                {/* <h1 className={styles.title}>Eminence Ensemble</h1> */}
+                <img className={style.logo} src={logo} alt="logo" />
             </div>
                 <NavBar></NavBar>
         </div>
