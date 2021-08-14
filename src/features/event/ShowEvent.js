ca7edc5c90594e5c143c6596028714234127698e
Zac <zacharyflynn06@gmail.com>
2021-08-14 13:50:41 -0600

    add navlink to edit form on events

--- src/features/event/ShowEvent.js
+++ src/features/event/ShowEvent.js
@@ -4,7 +4,7 @@ import { selectEvents , selectStatus } from './EventSlice';
 import { useParams } from 'react-router';
 import { useSelector } from 'react-redux';
 import Loading from '../loading/Loading';
-
+import { NavLink } from 'react-router-dom';
 import style from './Events.module.css'
 
 
@@ -37,6 +37,12 @@ const ShowEvent = () => {
                     {event.attributes.city},{event.attributes.state}
 
                 </div>
+
+                <NavLink
+                    to={`/events/${eventId}/edit`}
+                >
+                    <input type="button" value="Edit" />
+                </NavLink>
             </div>
         );
 
