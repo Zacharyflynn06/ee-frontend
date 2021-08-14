d8ce6ca7a886d761af89bbaf40f40db6b91ed5d1
Zac <zacharyflynn06@gmail.com>
2021-08-13 23:16:27 -0600

    add initial styles to Show Product page

--- src/features/shop/ShowProduct.js
+++ src/features/shop/ShowProduct.js
@@ -13,6 +13,10 @@ const ShowProduct = () => {
     const products = useSelector(selectProducts)
     const product = products.find(product => product.id === params.id)
 
+    const formatImageUrl = () => {
+        return product.attributes.image_format ? product.attributes.image_format.url : process.env.PUBLIC_URL + "logo192.png"
+    }
+
     if (status === "loading") {
 
         return (
@@ -24,8 +28,29 @@ const ShowProduct = () => {
 
             <div className={style.shopContainer}>
                 <NavBar />
-                <h1>show product</h1>
+
+                <div className={style.showProductContainer}>
                 <h2>Name: {product.attributes.name}</h2>
+
+                    <div className={style.showProductImageContainer}>
+                        <img className={style.showProductImage} src={formatImageUrl()} alt="" />
+                    </div>
+
+                    <div className={style.showProductDetails}>
+                        <div>
+
+                            <span>
+                                ${product.attributes.price}
+                            </span>
+
+                            <div>
+                                {product.attributes.description}
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+                
                 <NavLink
                     to={{
                         pathname: `/shop/products/${product.id}/edit`,
