14c738acb5e103d51b5eafd8e687d9435030b8e8
Zac <zacharyflynn06@gmail.com>
2021-08-17 00:15:51 -0600

    add basic style to contact page '

--- src/features/contact/Contact.js
+++ src/features/contact/Contact.js
@@ -2,17 +2,33 @@ import React from 'react';
 
 import style from './Contact.module.css'
 
-const Contact = (props) => {
+const Contact = () => {
     return (
         <div className={style.contactContainer}>
             <h1>Contact Page</h1>
-            <h2>Booking</h2>
-            <h3>Jordan Carriere</h3>
-            <h3>jordan@alpine-partners.net</h3>
+            <div className={style.contactWrapper}>
+                <div className={style.contactTitle}>
+                    Booking 
+                </div>
+                <div>
+                    Jordan Carriere
+                </div>
+                <div>
+                    jordan@alpine-partners.net
+                </div>
+            </div>
+            <div className={style.contactWrapper}>
+                <div className={style.contactTitle}>
+                    Management
+                </div>
+                <div>
+                    Tynan Conroy
+                </div>
+                <div>
+                    Mind Warp Management
+                </div>
 
-            <h2>Management</h2>
-            <h3>Tynan Conroy</h3>
-            <h3>Mind Warp Management</h3>
+            </div>
 
         </div>
     )
