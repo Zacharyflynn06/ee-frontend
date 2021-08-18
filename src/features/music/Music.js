fcdcc161552447e9e48c80e606b11ab4eadbdadd
Zac <zacharyflynn06@gmail.com>
2021-08-17 21:22:01 -0600

    add links to social icons and add color change and grow style

--- src/features/music/Music.js
+++ src/features/music/Music.js
@@ -8,25 +8,49 @@ const Music = (props) => {
             <h1>Music</h1>
      
             <div className={style.linkContainer}>
-                <div>
-                    <FontAwesomeIcon icon={['fab', 'apple']} size="4x" />
-                    Apple Music
+                <div className={style.apple}>
+                    <a target="_blank" rel="noreferrer" href="https://music.apple.com/us/artist/eminence-ensemble/980075367">
+                        <FontAwesomeIcon icon={['fab', 'apple']} size="4x" />
+                    <br />
+                    <span>Apple Music</span> 
+                    </a>
                 </div>
-                <div>
-                    <FontAwesomeIcon icon={['fab', 'spotify']} size="4x" />
-                    Spotify
+                <div className={style.spotify}>
+                    <a target="_blank" rel="noreferrer" href="https://open.spotify.com/artist/2gxgPqq1J8gV07X2Q1cIzv">
+                        <FontAwesomeIcon icon={['fab', 'spotify']} size="4x" />
+                        <br />
+                        <span>Spotify</span> 
+                    </a>
+                        
                 </div>
-                <div>
-                    <FontAwesomeIcon icon={['fab', 'amazon']} size="4x" />
-                    Amazon Music
+                <div className={style.youtube}>
+                    <a target="_blank" rel="noreferrer" href="https://www.youtube.com/channel/UCzGhe6JMn99ZGR9IDNEL4SA">
+                        <FontAwesomeIcon icon={['fab', 'youtube']} size="4x" />
+                        <br />
+                        <span>Youtube</span> 
+                    </a>
                 </div>
-                <div>
-                    <FontAwesomeIcon icon={['fab', 'deezer']} size="4x" />
-                    Deezer
+                <div className={style.amazon}>
+                    <a target="_blank" rel="noreferrer" href="https://music.amazon.com/artists/B018WC7HM6/eminence-ensemble?">
+                        <FontAwesomeIcon icon={['fab', 'amazon']} size="4x"  />
+                        <br />
+                        <span>AmazonMusic</span> 
+                    </a>
+                    
                 </div>
-                <div>
-                    <FontAwesomeIcon icon={['fab', 'bandcamp']} size="4x" />
-                    BandCamp
+                <div className={style.deezer}>
+                    <a target="_blank" rel="noreferrer" href="https://www.deezer.com/en/artist/9422360">
+                        <FontAwesomeIcon icon={['fab', 'deezer']} size="4x" />
+                        <br />
+                        <span>Deezer</span> 
+                    </a>
+                </div>
+                <div className={style.bandcamp}>
+                    <a target="_blank" rel="noreferrer" href="https://eminenceensemble.bandcamp.com/">
+                        <FontAwesomeIcon icon={['fab', 'bandcamp']} size="4x" />
+                        <br />
+                        <span>Bandcamp</span> 
+                    </a>
                 </div>
 
 
