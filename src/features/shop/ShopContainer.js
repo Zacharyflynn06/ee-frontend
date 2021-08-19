6918db548bba91f1a9e58a12af3e6d531662794c
Zac <zacharyflynn06@gmail.com>
2021-08-19 11:18:17 -0600

    remove unused dependencies from shopcontainer

--- src/features/shop/ShopContainer.js
+++ src/features/shop/ShopContainer.js
@@ -1,8 +1,7 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 // import ErrorPage from './ErrorPage'
-import NavBar from '../navbar/NavBar';
-import { clearState, shopSelector } from './ShopSlice';
-import { useDispatch, useSelector } from 'react-redux';
+import { shopSelector } from './ShopSlice';
+import { useSelector } from 'react-redux';
 import ProductCard from './ProductCard';
 import Loading from '../loading/Loading';
 
@@ -11,24 +10,15 @@ import style from './Shop.module.css'
 
 const ShopContainer = () => {
     
-    const dispatch = useDispatch()
+   
     const products = useSelector(shopSelector).products
     const {status} = useSelector(shopSelector)
-
-    // useEffect(() => {
-    //     return(
-    //         dispatch(clearState()),
-    //         console.log('shop is success', isSuccess)
-    //     )
-    // }, [dispatch, isSuccess]);
     
     if (status === 'loading') {
         
-        return (
-            <Loading />
-        )
+        return ( <Loading /> )
 
-    } else {
+    } else if (status === 'complete'){
         return(
             
             <div className={style.shopContainer}>
