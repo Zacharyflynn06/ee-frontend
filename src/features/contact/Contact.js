2cc20e00515d8bc2d107c1464ded34e28ff1acd2
Zac <zacharyflynn06@gmail.com>
2021-09-16 14:47:42 -0600

    add clickable email links to contact page

--- src/features/contact/Contact.js
+++ src/features/contact/Contact.js
@@ -3,16 +3,14 @@ import style from './Contact.module.css'
 const Contact = () => {
     return (
         <div className={style.contactContainer}>
-            <h1>Contact Page</h1>
+            <h1>Contact</h1>
             <div className={style.contactWrapper}>
                 <div className={style.contactTitle}>
                     Booking 
                 </div>
                 <div>
-                    Jordan Carriere
-                </div>
-                <div>
-                    jordan@alpine-partners.net
+                    <div>Jordan Carriere</div>
+                    <a href="mailto:jordan@alpine-partners.net">jordan@alpine-partners.net</a>  
                 </div>
             </div>
             <div className={style.contactWrapper}>
@@ -22,8 +20,15 @@ const Contact = () => {
                 <div>
                     Mind Warp Management
                 </div>
-                <div>Taylor Reed</div>
-                <div>Will Kertzman</div>
+                <div>
+                    <div>Taylor Reed</div>
+                    <a href="tank@mindwarpmgmt.com">tank@mindwarpmgmt.com</a>
+                </div>
+                <div>
+                    <div>Will Kertzman</div>
+                    <a href="mailto:will@mindwarpmgmt.com">will@mindwarpmgmt.com</a>
+                </div>
+                
                 <div>Tynan Conroy</div>
 
             </div>
