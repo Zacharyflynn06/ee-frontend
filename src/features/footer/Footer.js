be9dcbeadd3c9f9ad0535df7f632471b93b88dcc
Zac <zacharyflynn06@gmail.com>
2021-09-24 10:55:09 -0600

    add mailchimp component to footer

--- src/features/footer/Footer.js
+++ src/features/footer/Footer.js
@@ -1,11 +1,34 @@
 import style from './Footer.module.css'
+import MailchimpSubscribe from "react-mailchimp-subscribe"
+
+
+const SignupForm = ({ status, message, onValidated}) => {
+
+
+}
 
 const Footer = () => {
     return ( 
         <div className={style.footerContainer}>
+
+            <div className={style.signupContainer}>
+                <MailchimpSubscribe 
+                    url={process.env.REACT_APP_MAILCHIMP_URL}
+                    render={({ subscribe, status, message }) => (
+                        <SignupForm
+                            status={status} 
+                            message={message}
+                            onValidated={formData => subscribe(formData)}
+                        />
+                    )}
+                />
+
+            </div>
+
             <div className={style.copyright}>
                 <h4>&copy; Zac Flynn 2021</h4>
             </div>
+
         </div>
     );
 }
