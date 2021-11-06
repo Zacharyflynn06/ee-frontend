4101ff9fdad8993eb641e4d610f8d0e79bf453ef
Zachary Flynn <zacharyflynn@zacharys-mbp.lan>
2021-11-06 16:39:55 -0600

    refactor Contact page titles

--- src/features/contact/Contact.js
+++ src/features/contact/Contact.js
@@ -1,31 +1,44 @@
-import style from './Contact.module.css'
+import style from "./Contact.module.css";
 
 const Contact = () => {
-    return (
-        <div className={style.contactContainer}>
-            <h1>Contact</h1>
-            <div className={style.contactWrapper}>
-                <div className={style.contactTitle}>
+  return (
+    <div className={style.contactContainer}>
+      <h1>Contact</h1>
+      <div className={style.contactWrapper}>
+        {/* <div className={style.contactTitle}>
                     Booking 
-                </div>
-                <div className={style.contactDetails}>
-                    <div className={style.contactName}>Jordan Carriere</div>
-                    <a href="mailto:jordan@alpine-partners.net">jordan@alpine-partners.net</a>  
-                </div>
-                <div className={style.contactTitle}>
-                    Management
-                </div>
-                <div className={style.contactDetails}>
-                    <div className={style.contactName}>Taylor Reed</div>
-                    <a href="tank@mindwarpmgmt.com">tank@mindwarpmgmt.com</a>
-                </div>
-                <div className={style.contactDetails}>
-                    <div className={style.contactName}>Will Kertzman</div>
-                    <a href="mailto:will@mindwarpmgmt.com">will@mindwarpmgmt.com</a>
-                </div>
-            </div>
+                </div> */}
+        <div className={style.contactTitle}>
+          <h2>Booking</h2>
         </div>
-    )
-}
 
-export default Contact
+        <div className={style.contactDetails}>
+          <div className={style.contactName}>Jordan Carriere</div>
+          <a href="mailto:jordan@alpine-partners.net">
+            jordan@alpine-partners.net
+          </a>
+        </div>
+        {/* <div className={style.contactTitle}>
+                    Management
+                </div> */}
+        <div className={style.contactTitle}>
+          <h2>Management</h2>
+        </div>
+        <div className={style.contactDetails}>
+          <div className={style.contactName}>Taylor Reed</div>
+          <a href="tank@mindwarpmgmt.com">tank@mindwarpmgmt.com</a>
+        </div>
+        <div className={style.contactDetails}>
+          <div className={style.contactName}>Will Kertzman</div>
+          <a href="mailto:will@mindwarpmgmt.com">will@mindwarpmgmt.com</a>
+        </div>
+        <div className={style.contactDetails}>
+          <div className={style.contactName}>Tynan Conroy</div>
+          <a href="mailto:tynan@mindwarpmgmt.com">tynan@mindwarpmgmt.com</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- src/features/event/EventsContainer.js
+++ src/features/event/EventsContainer.js
@@ -1,55 +1,52 @@
-import style from './Events.module.css'
-import Loading from '../loading/Loading'
-import { eventSelector } from './EventSlice';
-import { userSelector } from '../User/userSlice';
-import { useSelector } from 'react-redux';
-import EventCard from './EventCard';
-import { NavLink } from 'react-router-dom';
+import style from "./Events.module.css";
+import Loading from "../loading/Loading";
+import { eventSelector } from "./EventSlice";
+import { userSelector } from "../User/userSlice";
+import { useSelector } from "react-redux";
+import EventCard from "./EventCard";
+import { NavLink } from "react-router-dom";
 
 const EventContainer = () => {
-    const {admin} = useSelector(userSelector)
-    const {status, events } = useSelector(eventSelector)
+  const { admin } = useSelector(userSelector);
+  const { status, events } = useSelector(eventSelector);
 
-    if (status === 'loading') {
-
-        return ( <Loading />)
-
-    } else {
-
-        const checkAdmin = () => {
-            if (admin) {
-                return (
-                    <div>
-                        <NavLink
-                            to={'/tour/events/new'}
-                        >
-                        <input type="button" value="Add Event"/>  
-                        </NavLink>
-    
-                    </div>
-                )
-            }    
-        }
-        
+  if (status === "loading") {
+    return <Loading />;
+  } else {
+    const checkAdmin = () => {
+      if (admin) {
         return (
-            <div className={style.eventsContainer}>
-                
-                <h1>Tour Dates</h1>
-                <div className={style.eventsTable}>
-                    {events.map(event => 
-    
-                        <EventCard
-                            event={event}
-                            key={event.id}
-                        />
-                    )}
-                </div>
-                {checkAdmin()}
-                
-            </div>     
+          <div>
+            <NavLink to={"/tour/events/new"}>
+              <input type="button" value="Add Event" />
+            </NavLink>
+          </div>
         );
+      }
+    };
+    if (events.length >= 1) {
+      return (
+        <div className={style.eventsContainer}>
+          <h1>Tour Dates</h1>
+          <div className={style.eventsTable}>
+            {events.map((event) => (
+              <EventCard event={event} key={event.id} />
+            ))}
+          </div>
+          {checkAdmin()}
+        </div>
+      );
+    } else {
+      return (
+        <div className={style.eventsContainer}>
+          <h1>Tour Dates</h1>
+          <div>
+            <h2>Check back soon for more tour dates!</h2>
+          </div>
+        </div>
+      );
     }
+  }
+};
 
-}
- 
 export default EventContainer;
