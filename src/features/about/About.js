536b5916d76f9caa059dfb62ef94d5a5427f6d34
Zachary Flynn <zacharyflynn@zacharys-mbp.lan>
2021-11-09 19:26:23 -0700

    add new bio

--- src/features/about/About.js
+++ src/features/about/About.js
@@ -1,41 +1,79 @@
-import style from './About.module.css'
+import style from "./About.module.css";
 
 const About = () => {
-    return (
-        <div className={style.aboutContainer}>
-            <h1>About</h1>
-            <div className={style.imageContainer}>
-                <img className={style.image} src={process.env.PUBLIC_URL + 'images/press-shot.jpg'}  alt="press-shot" />
-            </div>
+  return (
+    <div className={style.aboutContainer}>
+      <h1>About</h1>
+      <div className={style.imageContainer}>
+        <img
+          className={style.image}
+          src={process.env.PUBLIC_URL + "images/press-shot.jpg"}
+          alt="press-shot"
+        />
+      </div>
 
-            <div className={style.contentContainer}>
-                <div className={style.bioContainer}>
-                    <div className={style.bioContent}>
+      <div className={style.contentContainer}>
+        <div className={style.bioContainer}>
+          <div className={style.bioContent}>
+            <div className={style.bioTitle}>Biography</div>
 
-                        <div className={style.bioTitle}>Biography</div>
-                        <p>Since 2009, Eminence Ensemble has been building their fan-base through explosive live shows and unique genre-blending. The Colorado-based group infuses elements of Jazz, Rock, Electronic, Metal, Soul/Funk, and Hip-Hop to create intricate compositions, decorated with psychadelic improvisations</p>
-                       
-                        <p>The band is comprised of six members: Tanner Bardin [Drums], Nick Baum [Percussion/Lead Vocal], Wil Snyder [Keyboard/Synth], Zac Flynn [Bass/Synth/Vocals], Dylan Gleit [Guitar] & Justin Neely [Guitar/Vocals], making a solid framework for a huge sound and a whirlwind of creativity. </p>
-                        
-                        <p>Gaining their following first as a live band, Eminence Ensemble learned to capture the crowd with high-energy performances and large-scale production, with a light show just as dynamic as the music. And although they mainly thrive in the Jam Scene, the band is recognized by fans of all genres. Eminence Ensemble has become known for their complex song structures, heavy bass-drops, synchronized guitar-lines, dense vocal-harmonies, and powerful dual-percussion, both on-stage and in-studio.</p>
-                     
-                        <p>The sextet focuses most on creating a one-of-a-kind show for their followers. Each set list is carefully constructed to capture the vibe of the venue and the energy of the audience. With over 400 shows under their belt, Eminence Ensemble has proven their worth in the scene, playing festival slots at Summer Camp, Electric Forest, Camp Bisco, Wave Spell, Euphoria, The Werkout, and more.</p>
-                    </div>
-                </div>
-                <div className={style.galleryContainer}>
-                    <img src={process.env.PUBLIC_URL + 'images/gallery/1.jpg'} alt="1"/>
-                    <img src={process.env.PUBLIC_URL + 'images/gallery/2.jpg'} alt="2"/>
-                    <img src={process.env.PUBLIC_URL + 'images/gallery/3.jpg'} alt="3"/>
-                    <img src={process.env.PUBLIC_URL + 'images/gallery/4.jpg'} alt="4"/>
-                    <img src={process.env.PUBLIC_URL + 'images/gallery/5.jpg'} alt="5"/>
-                    <img src={process.env.PUBLIC_URL + 'images/gallery/6.jpg'} alt="6"/>
-                    <img src={process.env.PUBLIC_URL + 'images/gallery/7.jpg'} alt="7"/>
-                    <img src={process.env.PUBLIC_URL + 'images/gallery/8.jpg'} alt="8"/>
+            <p>
+              Since 2009, Eminence Ensemble has provided people from all walks
+              of life with a sense of community, a safe space for expression, a
+              needed break from the pressures of life, and a thrilling rush of
+              adrenaline through complex, yet digestible music that bridges the
+              gap between the jam-band and more traditional music scenes.
+            </p>
 
-                </div>
-            </div>
+            <p>
+              The Colorado-based sextet, comprised of members Tanner Bardin
+              [drums], Nick Baum [vocals, percussion], Wil Snyder [keyboard,
+              synth], Zac Flynn [bass, synth, vocals], Dylan Gleit [guitar,
+              vocals] & Justin Neely [guitar, vocals], has been steadily
+              building their fan-base through explosive live shows featuring a
+              unique conglomerate of genre-blending compositions. The
+              archipelago of sonic worlds put forth by the band features
+              elements of rock, funk, metal, electronic, soul, and hip-hop that
+              together create an intricate foundation to be decorated by
+              exploratory improvisational jams in a unique way, every show.
+            </p>
+
+            <p>
+              Gaining their following first as a live band, Eminence Ensemble
+              learned to capture the crowd with high-energy performances and
+              large-scale production, with a light show just as dynamic as the
+              music. Although they mainly thrive in the jam scene, the band is
+              recognized and loved by fans of all genres. Eminence Ensemble has
+              become known for their complex song structures, harmonized
+              dual-guitar lines, heavy bass drops, dense vocal harmonies, and
+              powerful percussion, both on stage and in the studio.
+            </p>
+
+            <p>
+              The sextet focuses most on creating a one-of-a-kind show for their
+              fans each night. Every set list is carefully constructed to
+              capture the vibe of the night, resulting in a continuous energy
+              exchange between the band and audience that strengthens with every
+              cycle. With over 400 shows under their belt, Eminence Ensemble has
+              proven their worth in the scene, playing festival slots at Summer
+              Camp, Electric Forest, Camp Bisco, Wave Spell, Euphoria, Gem &
+              Jam, The Werkout, Same Same But Different, and many others.
+            </p>
+          </div>
         </div>
-    )
-}
+        <div className={style.galleryContainer}>
+          <img src={process.env.PUBLIC_URL + "images/gallery/1.jpg"} alt="1" />
+          <img src={process.env.PUBLIC_URL + "images/gallery/2.jpg"} alt="2" />
+          <img src={process.env.PUBLIC_URL + "images/gallery/3.jpg"} alt="3" />
+          <img src={process.env.PUBLIC_URL + "images/gallery/4.jpg"} alt="4" />
+          <img src={process.env.PUBLIC_URL + "images/gallery/5.jpg"} alt="5" />
+          <img src={process.env.PUBLIC_URL + "images/gallery/6.jpg"} alt="6" />
+          <img src={process.env.PUBLIC_URL + "images/gallery/7.jpg"} alt="7" />
+          <img src={process.env.PUBLIC_URL + "images/gallery/8.jpg"} alt="8" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- src/features/music/Music.js
+++ src/features/music/Music.js
@@ -1,67 +1,85 @@
-import style from './Music.module.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import style from "./Music.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Music = (props) => {
-    return ( 
-        <div className={style.musicContainer}>
-            <h1>Music</h1>
-            <div className={style.linkContainer}>
-                <div className={style.linkTitle}>
-                    <h2>Listen Now!</h2>
-                </div>
-                <div className={style.links}>
-                    <div className={style.apple}>
-                        <a target="_blank" rel="noreferrer" href="https://music.apple.com/us/artist/eminence-ensemble/980075367">
-                            <FontAwesomeIcon icon={['fab', 'apple']} size="4x" />
-                        <br />
-                        <span>Apple Music</span> 
-                        </a>
-                    </div>
-                    <div className={style.spotify}>
-                        <a target="_blank" rel="noreferrer" href="https://open.spotify.com/artist/2gxgPqq1J8gV07X2Q1cIzv">
-                            <FontAwesomeIcon icon={['fab', 'spotify']} size="4x" />
-                            <br />
-                            <span>Spotify</span> 
-                        </a>
-                            
-                    </div>
-                    <div className={style.youtube}>
-                        <a target="_blank" rel="noreferrer" href="https://www.youtube.com/channel/UCzGhe6JMn99ZGR9IDNEL4SA">
-                            <FontAwesomeIcon icon={['fab', 'youtube']} size="4x" />
-                            <br />
-                            <span>Youtube</span> 
-                        </a>
-                    </div>
-                    <div className={style.amazon}>
-                        <a target="_blank" rel="noreferrer" href="https://music.amazon.com/artists/B018WC7HM6/eminence-ensemble?">
-                            <FontAwesomeIcon icon={['fab', 'amazon']} size="4x"  />
-                            <br />
-                            <span>Amazon Music</span> 
-                        </a>
-                        
-                    </div>
-                    <div className={style.deezer}>
-                        <a target="_blank" rel="noreferrer" href="https://www.deezer.com/en/artist/9422360">
-                            <FontAwesomeIcon icon={['fab', 'deezer']} size="4x" />
-                            <br />
-                            <span>Deezer</span> 
-                        </a>
-                    </div>
-                    <div className={style.bandcamp}>
-                        <a target="_blank" rel="noreferrer" href="https://eminenceensemble.bandcamp.com/">
-                            <FontAwesomeIcon icon={['fab', 'bandcamp']} size="4x" />
-                            <br />
-                            <span>Bandcamp</span> 
-                        </a>
-                    </div>
-
-                </div>
-
-
-            </div>
-
+  return (
+    <div className={style.musicContainer}>
+      <h1>Music</h1>
+      <div className={style.linkContainer}>
+        <div className={style.linkTitle}>
+          <h2>Listen Now!</h2>
         </div>
-    );
-}
- 
+        <div className={style.links}>
+          <div className={style.bandcamp}>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://eminenceensemble.bandcamp.com/"
+            >
+              <FontAwesomeIcon icon={["fab", "bandcamp"]} size="4x" />
+              <br />
+              <span>Bandcamp</span>
+            </a>
+          </div>
+          <div className={style.apple}>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://music.apple.com/us/artist/eminence-ensemble/980075367"
+            >
+              <FontAwesomeIcon icon={["fab", "apple"]} size="4x" />
+              <br />
+              <span>Apple Music</span>
+            </a>
+          </div>
+          <div className={style.spotify}>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://open.spotify.com/artist/2gxgPqq1J8gV07X2Q1cIzv"
+            >
+              <FontAwesomeIcon icon={["fab", "spotify"]} size="4x" />
+              <br />
+              <span>Spotify</span>
+            </a>
+          </div>
+          <div className={style.youtube}>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.youtube.com/channel/UCzGhe6JMn99ZGR9IDNEL4SA"
+            >
+              <FontAwesomeIcon icon={["fab", "youtube"]} size="4x" />
+              <br />
+              <span>Youtube</span>
+            </a>
+          </div>
+          <div className={style.amazon}>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://music.amazon.com/artists/B018WC7HM6/eminence-ensemble?"
+            >
+              <FontAwesomeIcon icon={["fab", "amazon"]} size="4x" />
+              <br />
+              <span>Amazon Music</span>
+            </a>
+          </div>
+          <div className={style.deezer}>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.deezer.com/en/artist/9422360"
+            >
+              <FontAwesomeIcon icon={["fab", "deezer"]} size="4x" />
+              <br />
+              <span>Deezer</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Music;
