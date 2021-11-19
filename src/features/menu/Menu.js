e05b1251b027def22cb508d6460d852b65bf5339
Zac Flynn <zacharyflynn06@gmail.com>
2021-11-18 22:38:39 -0700

    change input to button

--- src/features/menu/Menu.js
+++ src/features/menu/Menu.js
@@ -1,37 +1,41 @@
-import React, {useState} from 'react'
-import { useLocation } from 'react-router-dom';
-import Home from '../home/Home';
-import style from './Menu.module.css'
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+import Home from "../home/Home";
+import style from "./Menu.module.css";
 
 const Menu = () => {
-    const location = useLocation()
-    const [isActive, setActive] = useState(false)
+  const location = useLocation();
+  const [isActive, setActive] = useState(false);
 
-    // const dispatch = useDispatch()
+  // const dispatch = useDispatch()
 
-    const toggleMenu = () => {
-        isActive ? setActive(false) : setActive(true)
-    }
+  const toggleMenu = () => {
+    isActive ? setActive(false) : setActive(true);
+  };
 
-    if (location.pathname === '/') {
-        return ( <></>)
-    } else {
-        return ( 
-            <div className={isActive ? style.activeMenuWrap : style.menuWrap}>
-                <input type="button" id={style.toggler} className={isActive ? style.active : ""} onClick={toggleMenu}/>
-                <div className={style.hamburger}><div></div></div>
-                <div className={style.menu}>
-                    <div>
-                        <div>
-                            <Home toggleMenu={toggleMenu}/>
-                        </div>
-                    </div>
-                </div>
+  if (location.pathname === "/") {
+    return <></>;
+  } else {
+    return (
+      <div className={isActive ? style.activeMenuWrap : style.menuWrap}>
+        <button
+          id={style.toggler}
+          className={isActive ? style.active : ""}
+          onClick={toggleMenu}
+        />
+        <div className={style.hamburger}>
+          <div></div>
+        </div>
+        <div className={style.menu}>
+          <div>
+            <div>
+              <Home toggleMenu={toggleMenu} />
             </div>
-        );
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
 
-    }
-
-}
- 
 export default Menu;
