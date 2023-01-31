aaa9474fabf18a47cd27acb6f243fcadbf3e9674
Zac Flynn <zacharyflynn06@gmail.com>
2023-01-31 15:35:52 -0700

    update booking contact

--- src/features/contact/Contact.js
+++ src/features/contact/Contact.js
@@ -13,9 +13,8 @@ const Contact = () => {
         </div>
 
         <div className={style.contactDetails}>
-          <div className={style.contactName}>Jordan Carriere</div>
-          <a href="mailto:jordan@alpine-partners.net">
-            jordan@alpine-partners.net
+          <a href="mailto:Tanner@worldmusicdevelopment.org">
+          Tanner@worldmusicdevelopment.org
           </a>
         </div>
         {/* <div className={style.contactTitle}>
@@ -24,22 +23,7 @@ const Contact = () => {
         <div className={style.contactTitle}>
           <h2>Management</h2>
         </div>
-        {/* <div className={style.contactDetails}>
-          <div className={style.contactName}>Taylor Reed</div>
-          <a href="tank@mindwarpmgmt.com">tank@mindwarpmgmt.com</a>
-        </div>
-        <div className={style.contactDetails}>
-          <div className={style.contactName}>Will Kertzman</div>
-          <a href="mailto:will@mindwarpmgmt.com">will@mindwarpmgmt.com</a>
-        </div>
-        <div className={style.contactDetails}>
-          <div className={style.contactName}>Tynan Conroy</div>
-          <a href="mailto:tynan@mindwarpmgmt.com">tynan@mindwarpmgmt.com</a>
-        </div> */}
 
-        {/* <div className={style.contactTitle}>
-          <h2>Business Manager</h2>
-        </div> */}
         <div className={style.contactDetails}>
           <div className={style.contactName}>Jason Morris</div>
           <a href="jason@eminenceensemble.com">jason@eminenceensemble.com</a>
