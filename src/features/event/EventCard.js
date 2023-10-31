7c26fa3135fcb7dc178f354505c2cd1889b4b844
Zac Flynn <zacharyflynn06@gmail.com>
2023-10-30 18:36:47 -0600

    fix time not showing

--- src/features/event/EventCard.js
+++ src/features/event/EventCard.js
@@ -2,19 +2,23 @@ import style from "./Events.module.css";
 // import { NavLink } from "react-router-dom";
 
 const EventCard = (props) => {
+  console.log(props);
   const { datetime, offers, venue } = props.event;
   const dateObj = new Date(datetime).toLocaleDateString("en-us", {
-    year: "numeric",
     month: "short",
     day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
   });
 
-  const isTicketLink = offers?.[0]?.url
+  const isTicketLink = offers?.[0]?.url;
   return (
     <div className={style.eventCard}>
       <div className={style.eventDescription}>
         {/* <NavLink to={`/tour/events/${id}`} exact> */}
-        <div className={style.eventDateCell}>{dateObj} - {venue.location}</div>
+        <div className={style.eventDateCell}>
+          {venue.location} - {dateObj}
+        </div>
         <div className={style.eventVenueCell}>
           <div className={style.eventVenueName}>{venue.name}</div>
         </div>
@@ -22,16 +26,18 @@ const EventCard = (props) => {
         {/* </NavLink> */}
       </div>
 
-      {isTicketLink ? (      <a
-        className={style.eventLink}
-        target="_blank"
-        rel="noreferrer"
-        href={offers[0].url}
-      >
-        Tickets
-      </a>) : (<div className={style.eventLink}>Coming Soon</div>)}
-
-      
+      {isTicketLink ? (
+        <a
+          className={style.eventLink}
+          target="_blank"
+          rel="noreferrer"
+          href={offers[0].url}
+        >
+          Tickets
+        </a>
+      ) : (
+        <div className={style.eventLink}>Free</div>
+      )}
     </div>
   );
 };
